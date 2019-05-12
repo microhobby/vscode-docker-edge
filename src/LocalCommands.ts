34650af979a3f14416bb9455029a1d5198c8c80d
Matheus Castello <matheus.castello@toradex.com>
2019-05-12 13:51:03 -0300

    LocalCommands: Add execDockerFileFromRegistry
    This method build Dockerfile, push to localhost registry and pull it
    on device
    
    Signed-off-by: Matheus Castello <matheus.castello@toradex.com>

--- src/LocalCommands.ts
+++ src/LocalCommands.ts
@@ -39,6 +39,71 @@ export class LocalCommands {
 		});
 	}
 
+	execDockerFileFromRegistry(cbk: any): void {
+		let out: vscode.OutputChannel = 
+			vscode.window.createOutputChannel("Torizon Push");
+		let apps: string = this.appName;
+		let locals: string = this.localPath;
+		let ips: string = this.ip;
+
+		let child: any = cp.exec(
+			`docker build -f ${this.localPath}/Dockerfile ${this.localPath} -t torizon/${apps}`,
+			(err: any, stdout: string, stderr: string) => {
+				if (err) {
+					vscode.window.showErrorMessage(stderr);
+				} else {
+					vscode.window
+						.showInformationMessage("Packing image ....");
+					out.appendLine("******************* Running Saving Image ********************");
+					let child2: any = cp.exec(
+						`docker tag torizon/${apps} localhost:5000/torizon/${apps} && docker push localhost:5000/torizon/${apps}`,
+						(err: any, stdout: string, stderr: string) => {
+							if (err) {
+								console.log(stderr);
+								vscode.window.showErrorMessage("Error trying to save image 🤔");
+							} else {
+								let ssh = new SSHCommands(ips);
+								ssh.runDockerWithMyRegistry().then(ret => {
+									if (ret) {
+										out.clear();
+										out.appendLine("******************* Running Pushing Image ********************");
+										out.append("⌚ Pushing ");
+										let intv = setInterval(() => {
+											out.append(".");
+										}, 500);
+
+										let ssh1 = new SSHCommands(ips);
+										ssh1.pullImageFromMyRegistry(`torizon/${apps}`, out).then(ret => {
+											clearInterval(intv);
+											out.hide();
+
+											if (ret && cbk) {
+												vscode.window.showInformationMessage("Image loaded 😎");
+												cbk(ret);
+											} else {
+												vscode.window.showErrorMessage("Error trying to push image 🤔");
+											}
+										});
+									} else {
+										vscode.window.showErrorMessage("Error trying to communicate with device 🤔");
+									}
+								});
+							}
+						}
+					);
+				}
+			}
+		);
+
+		out.show();
+		out.appendLine("******************* Running Torizon Push ********************");
+		vscode.window.showInformationMessage("building Dockerfile ...");
+
+		child.stdout.on('data', function(data: any) {
+			out.append(data.toString());
+		});
+	}
+
 	execDockerFile(cbk: any): void {
 		/* TODO PLEASE REFACTOR THIS CALLBACK HELL use promises */
 		let out: vscode.OutputChannel = vscode.window.createOutputChannel("Torizon Push");
